d18f4334b760a22ba179d423c799a27a3e439a64
melika0000 <mmelika113@gmail.com>
2023-02-21 17:29:30 +0330

    responsive nav

---
+++ app.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("navMenu").style.visibility = "visible";
+    document.getElementById("navMenu").style.opacity = "1";
+}
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("navMenu").style.visibility = "hidden";
+    document.getElementById("navMenu").style.opacity = "0";
+}
