71359f59e86c16afa1ff56f52926f19f72e96feb
melika0000 <mmelika113@gmail.com>
2023-02-24 01:43:53 +0330

    auto slider

--- app.js
+++ app.js
@@ -31,3 +31,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   slideNav[slideIndex-1].className += " active";
 }
+
+showSlidesAuto();
+
+function showSlidesAuto() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlidesAuto, 6000); // Change image every 2 seconds
+}
