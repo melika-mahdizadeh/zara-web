866cbc54c52002b9b084b65495aa62d6526e3c89
melika0000 <mmelika113@gmail.com>
2023-02-23 22:51:27 +0330

    added main slider with slidenav buttons

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ function closeNav() {
     document.getElementById("navMenu").style.visibility = "hidden";
     document.getElementById("navMenu").style.opacity = "0";
 }
+/* slider */
+let slideIndex = 1;
+showSlides(slideIndex);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let slideNav = document.getElementsByClassName("slide-nav");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < slideNav.length; i++) {
+    slideNav[i].className = slideNav[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  slideNav[slideIndex-1].className += " active";
+}
